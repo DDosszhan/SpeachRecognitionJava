ab40bad33dccf59db50d6dbac840f6bea8de444d
doszhan <doszhankonysuly@gmail.com>
2024-10-01 03:35:24 +0500

    nice!

--- src/main/java/org/webapp/App.java
+++ src/main/java/org/webapp/App.java
@@ -1,13 +1,34 @@
 package org.webapp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.assemblyai.api.AssemblyAI;
+import com.assemblyai.api.resources.transcripts.types.*;
+
+public final class App {
+
+    public static void main(String... args) throws Exception {
+        AssemblyAI client = AssemblyAI.builder()
+                .apiKey("API_KEY_GOES_HERE")
+                .build();
+
+
+        // Song "Atlantis" by Seafret
+        String url = "https://drive.google.com/uc?export=download&id=1CEfzmfo1Aaz5f7OjNy54Vb-1n_AEfe26";
+
+        var config = TranscriptOptionalParams.builder()
+                .speakerLabels(true)
+                .build();
+
+
+        Transcript transcript = client.transcripts().transcribe(url, config);
+
+        if (transcript.getStatus() == TranscriptStatus.ERROR) {
+            throw new Exception("Transcript failed with error: " + transcript.getError().get());
+        }
+
+        System.out.println("Transcript: " + transcript.getText());
+
+        transcript.getUtterances().ifPresent(utterances ->
+                utterances.forEach(utterance ->
+                        System.out.println("Speaker" + utterance.getSpeaker()+ ": " + utterance.getText())));
     }
 }
